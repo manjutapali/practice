9dc699ae109f14f22a211fdd79d4501b2735c4c1
manjutapali <tmanjunath04@gmail.com>
2017-09-07 15:24:59 +0530

    Refactored the code

---
+++ Lists/LinkedListDriver.java
@@ -0,0 +1,17 @@
+public class LinkedListDriver{
+
+    public static void main(String[] args)
+    {
+        LinkedList l = new LinkedList();
+
+        l.add(10);
+        l.add(20);
+        l.add(40);
+        l.add(50);
+        l.add(60);
+        l.add(70);
+        l.display();
+        l.reverse();
+        l.display();
+    }
+}
