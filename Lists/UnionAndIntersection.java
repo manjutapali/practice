67a62c89c28452431e2cbee9614292aa2f970e9c
manjutapali <tmanjunath04@gmail.com>
2017-10-16 22:07:20 +0530

    Union and intersection methods

---
+++ Lists/UnionAndIntersection.java
@@ -0,0 +1,15 @@
+/*
+	1. Keep one pointers for each list, compare the lists to check for the data.
+	2. Sort the list, with merge sort and compare the list for data.
+	3. Keep set, add a list, and check does set has the element 
+*/
+
+import java.util.HashSet;
+
+public class UnionAndIntersection
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
