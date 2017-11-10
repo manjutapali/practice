77a71dbebce3163e63c99be3093c49a5130cc51b
manjutapali <tmanjunath04@gmail.com>
2017-11-10 18:03:43 +0530

    Max number of platforms required in the station, based on arrival and departure

---
+++ Implementations/NumberOfPlatsRequired.py
@@ -0,0 +1,34 @@
+#code
+
+def NumPlatRequired(arr, dep):
+    dep.sort();
+    arr.sort();
+    plat_need = 1;
+    plat_max = 1;
+    i = 1; j = 0;
+    n = len(arr)
+    
+    while (i < n) and (j < n):
+        
+        if(arr[i] < dep[j]):
+            plat_need += 1
+            i += 1;
+            if(plat_max < plat_need):
+                plat_max = plat_need;
+                
+        else:
+            plat_need -= 1
+            j += 1;
+    
+    return plat_max;
+
+T = int(input())
+
+for i in range(0, T):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    dep = list(map(int, input().split()))
+    platforms = NumPlatRequired(arr, dep)
+    print(platforms)
+            
+    
